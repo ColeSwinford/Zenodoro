58f6f3d67fbf81acd0d35075832e74b265a8cbb0
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2022-11-04 18:09:27 -0400

    Added alert, alert sound, and alert tab emoji

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const timerElement = document.getElementById("timer");
-focusMinutes = 50;
-coolDownMinutes = 10;
+focusMinutes = 50; // Default 50
+coolDownMinutes = 10; // Default 10
 time = focusMinutes * 60;
 minutes = Math.floor(time/60);
 let seconds = time % 60;
@@ -25,12 +25,37 @@ function coolDownRunning(){
     return timerRun, coolDownRun;
 }
 
+// Alert
+function sound(){
+    let time = 0;
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? `0` + seconds : seconds;
+    timerElement.innerHTML = `${minutes}:${seconds}`;
+    tabTimerElement.innerHTML = "Zenodoro | &#9200;";
+    audio = document.getElementById('zenAlert');
+    audio.loop = true;
+    audio.currentTime = 0;
+    audio.play();
+    alert("Timer has ended. Click 'OK' to start next timer.");
+    audio.pause();
+
+    if(timerRun == true){
+        clearIntervals();
+        return coolDown();
+    }
+
+    if(coolDownRun == true){
+        clearIntervals();
+        resetTimer();
+        return timer();
+    }
+}
 
 //Clear Intervals
 function clearIntervals(){
     return clearInterval(timerInterval);
 }
-
 
 //Focus Timer
 function timer(){
@@ -40,7 +65,7 @@ function timer(){
     function updateTimer(){
         if(time == -1){
             clearIntervals();
-            coolDown();
+            sound();
         }
         let minutes = Math.floor(time/60);
         let seconds = time % 60;
@@ -73,8 +98,7 @@ function coolDown(){
     function updateTimer(){
         if(time == -1){
             clearIntervals();
-            resetTimer();
-            timer();
+            sound();
         }
         let minutes = Math.floor(time/60);
         let seconds = time % 60;
@@ -94,7 +118,6 @@ function resetCoolDown(){
     seconds = seconds < 10 ? `0` + seconds : seconds;
     return minutes, seconds;
 }
-
 
 //Controls
 function pause(){
