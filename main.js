2becf47835073be35dde91d6ef72a31984ed947f
Cole Swinford <19520329+ColeSwinford@users.noreply.github.com>
2022-11-20 15:57:48 -0500

    custom timer

--- main.js
+++ main.js
@@ -164,3 +164,15 @@ function next(){
         return restart();
     }
 };
+
+// Settings
+function showSettings(){
+    document.getElementById('cog').style.display="none";
+    document.getElementById('cogPressed').style.display="flex";
+    document.getElementById('settingsForm').style.display="flex"
+}
+function hideSettings(){
+    document.getElementById('cog').style.display="flex";
+    document.getElementById('cogPressed').style.display="none";
+    document.getElementById('settingsForm').style.display="none"
+}
